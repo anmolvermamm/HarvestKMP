e834de4dab488ed746373aa77ff02c56e9224726
Nasir Ahmed Momin <nasir.momin@mutualmobile.com>
2022-05-24 10:42:02 +0530

    Design Login view as per mock frame

--- iosApp/iosApp/Views/LoginView.swift
+++ iosApp/iosApp/Views/LoginView.swift
@@ -10,8 +10,94 @@ import Foundation
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text("Login View")
+        ScrollView {
+            Spacer().frame(height: UIScreen.main.bounds.height * 0.1)
+            googleSignInButton
+            LabelledDivider(label: "or", color: ColorAssets.white.color)
+            credentialView
+            footerView
+        }
+        .frame(width: UIScreen.main.bounds.width,
+               height: UIScreen.main.bounds.height,
+               alignment: .center)
+        .background(ColorAssets.colorBackground.color)
+        .edgesIgnoringSafeArea(.all)
+    }
+    
+    private var googleSignInButton: some View {
+        Button {
+            // TODO: (Nasir) Need to handle
+        } label: {
+            HStack {
+                Image("") // TODO: (Nasir) add Google icon
+                Text("Sign In with Google")
+            }
+            .foregroundColor(.black)
+            .font(.title3)
+            .padding()
+            .frame(width: UIScreen.main.bounds.width - 70)
+            .background(ColorAssets.white.color)
+            .cornerRadius(10.0)
+        }
+        .padding(.bottom)
+    }
+    
+    private var credentialView: some View {
+        VStack {
+            VStack {
+                TextField("Work Email", text: $email)
+                    .padding(.bottom)
+                SecureField("Password", text: $password)
+            }
+            .padding()
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            signinButton
+        }
+        .padding()
+    }
+    
+    private var signinButton: some View {
+        Button {
+            // TODO: (Nasir) Handle action
+        } label: {
+            Text("Sign In")
+                .foregroundColor(.black)
+                .font(.title3)
+                .padding()
+                .frame(width: UIScreen.main.bounds.width - 70)
+                .background(ColorAssets.white.color)
+            
+        }
+        .cornerRadius(10.0)
+    }
+    
+    var footerView: some View {
+        VStack {
+            HStack {
+                Text("Don't have an account?")
+                Button {
+                    // TODO: (Nasir) Handle action
+                } label: {
+                    Text("Try Harvest Free")
+                        .font(.headline)
+                }
+            }
+            .padding(.bottom)
+            
+            Button {
+                // TODO: (Nasir) Handle action
+            } label: {
+                Text("View Tour")
+                    .font(.title3)
+            }
+        }
+        .foregroundColor(ColorAssets.white.color)
     }
 }
 
@@ -22,3 +108,34 @@ struct LoginView_Previews: PreviewProvider {
     }
 }
 #endif
+
+// From: https://stackoverflow.com/questions/56619043/show-line-separator-view-in-swiftui
+struct LabelledDivider: View {
+    
+    let label: String
+    let horizontalPadding: CGFloat
+    let color: Color
+    
+    init(label: String, horizontalPadding: CGFloat = 10, color: Color = .black) {
+        self.label = label
+        self.horizontalPadding = horizontalPadding
+        self.color = color
+    }
+    
+    var body: some View {
+        HStack {
+            line
+            Text(label)
+                .foregroundColor(color)
+            line
+        }.padding(.horizontal)
+    }
+    
+    var line: some View {
+        VStack {
+            Divider()
+                .background(color)
+        }
+        .padding(horizontalPadding)
+    }
+}
